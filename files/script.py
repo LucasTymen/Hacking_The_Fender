73437c8d72c1d75ac2253604eba972a42ab5fbca
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-14 19:06:13 +0200

    Hacking The Fender_step_06

---
+++ files/script.py
@@ -0,0 +1,6 @@
+import csv
+compromised_users = []
+with open('passwords.csv') as password_file:
+  password_csv = csv.DictReader(password_file)
+  for password_row in password_csv:
+    print(password_row['Username'])
