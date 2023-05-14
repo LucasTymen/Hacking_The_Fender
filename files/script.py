2d762ac85a36da1a468688220dfc30c7075fef0d
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-14 23:00:45 +0200

    Hacking The Fender_step_15

--- files/script.py
+++ files/script.py
@@ -1,4 +1,6 @@
 import csv
+import json
+
 compromised_users = []
 with open('passwords.csv') as password_file:
   password_csv = csv.DictReader(password_file)
@@ -9,3 +11,10 @@ with open('passwords.csv') as password_file:
 with open('compromised_users.txt', 'w') as compromised_user_file:
   for compromised_user in compromised_users:
     compromised_user_file.write(compromised_user)
+
+with open('boss_message.json', 'w') as boss_message:
+  boss_message_dict = {
+    'recipient':'The Boss',
+    'message':'Mission Success'
+    }
+  json.dump(boss_message_dict ,boss_message)
